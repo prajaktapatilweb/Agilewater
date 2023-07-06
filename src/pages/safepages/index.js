f5a02264def67b4d1d08908c40bc651d2a532f40
Prajakta_Laptop <prajaktaweb@gmail.com>
2023-07-06 20:52:37 +0530

    Build Error removed

--- src/pages/safepages/index.js
+++ src/pages/safepages/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import AppPage from '../../@crema/hoc/UserPage';
-import asyncComponent from '../../@crema/utility/asyncComponent';
+// import React from 'react';
+// import AppPage from '../../@crema/hoc/UserPage';
+// import asyncComponent from '../../@crema/utility/asyncComponent';
 
-const Crypto = asyncComponent(() => import('../../modules/safepages'));
-export default AppPage(() => <Crypto />);
+// const Crypto = asyncComponent(() => import('../../modules/safepages'));
+// export default AppPage(() => <Crypto />);
+import React from 'react';
+
+export default function index() {
+  return <div>index</div>;
+}
