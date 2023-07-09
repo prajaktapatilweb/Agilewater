09c8f6dcdd665e731aab4326759b9c8277848b51
prajaktapatilweb <prajaktaweb@gmail.com>
2023-07-09 11:54:21 +0530

    After merging Footer change 2

--- src/pages/safepages/index.js
+++ src/pages/safepages/index.js
@@ -1,10 +1,7 @@
-// import React from 'react';
-// import AppPage from '../../@crema/hoc/UserPage';
-// import asyncComponent from '../../@crema/utility/asyncComponent';
-
-// const Crypto = asyncComponent(() => import('../../modules/safepages'));
-// export default AppPage(() => <Crypto />);
 import React from 'react';
+import AppPage from '../../@crema/hoc/UserPage';
+import asyncComponent from '../../@crema/utility/asyncComponent';
+
 
 const Crypto = asyncComponent(() => import('../../modules/safepages/SafeAgileCert'));
 export default AppPage(() => <Crypto />);
