c69f0efa361911fedf189964fa5639120669bccd
Prajakta_Laptop <prajaktaweb@gmail.com>
2023-07-09 14:04:47 +0530

    Testing Image Display Problem 10

--- src/modules/safepages/SafeAgileCert/Imagepara.js
+++ src/modules/safepages/SafeAgileCert/Imagepara.js
@@ -9,7 +9,8 @@ export default function Imagepara() {
   return (
     <div>
       <Herosection
-        image1='/assets/images/Safe/SAFe-Agilist-Logo.png'
+        image1='/assets/images/safe/ICP-ACC.webp'
+        // /assets/images/Safe/SAFe-Agilist-Logo.png'
         heading=' Leading SAFe® 6.0 / SAFe® 6.0 Agilist Certification'
         list1={[
           '2 Days of Classroom Training',
