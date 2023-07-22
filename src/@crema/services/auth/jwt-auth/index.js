1f6b3e6f7173b708c2c8e1f60a03e3c08058fec5
Prajakta_Laptop <prajaktaweb@gmail.com>
2023-07-22 22:55:48 +0530

    BaseURL added for dev and prd

--- src/@crema/services/auth/jwt-auth/index.js
+++ src/@crema/services/auth/jwt-auth/index.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 console.log('On hit');
 const jwtAxios = axios.create({
   // baseURL: 'https://crema-mongo-api.herokuapp.com/api/', // YOUR_API_URL HERE
-  baseURL: 'http://localhost:4000/api/', // YOUR_API_URL HERE
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:4000/api/'
+      : 'https://agilewater.vercel.app/api', // YOUR_API_URL HERE
   headers: {
     'Content-Type': 'application/json',
   },
