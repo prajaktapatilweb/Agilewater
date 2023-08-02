491bfea0943894e50889120ca446f556378f2067
prajaktapatilweb <prajaktaweb@gmail.com>
2023-08-02 18:30:58 +0530

    scrum master page done

--- src/modules/AdvScrumMaster/AdvSMaccord.js
+++ src/modules/AdvScrumMaster/AdvSMaccord.js
@@ -73,6 +73,125 @@ export default function AdvSMaccord() {
                 </span>
             ),
         },
+        {
+            heading: 'What are the objectives of this course?',
+            para: (
+                <span>
+                    <List>
+
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon><span><b>Facilitate Better Dialogue:</b> As a Scrum Master, it is a must needed skill which one needs to have to have an immediate response to the challenges and that too with appropriate guidance of the Scrum team. With this objective, we at Agilewaters will train you on how to have effective communication for better facilitation of dialogue between Scrum teams, product owners, stakeholders, executives and customers to have a better end to end delivery.</span></ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon><span><b>Confident Responsiveness:</b> While working in an organisation we come across various challenges, which demands our confident response with an effective approach. With this objective, we at Agilewaters will train you with techniques which come best in purpose to resist the change, sometimes in lack of engagement, sometimes in low motivation and sometimes during unavailability of key people.</span></ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon><span><b>Increasing the engagement:</b> The more you are having the engagement, the more productive results come out in the end. As a Scrum Master, it becomes the necessity as a leader and mentor of the Scrum team to keep the team engaged with productivity and providing solutions to the enterprise. With this objective, we at Agilewaters will train you so you can have greater accountability, much engagement, high buy-ins and effective commitment.</span></ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon><span><b>Scale the Scrum:</b> As a Scrum Master, one needs to be a master at scaling the scrum framework across various teams within an organisation. With this objective, we at Agilewaters will train you on techniques to scale the Scrum within the various teams to have exquisite results and processes which makes the workflow much and more agile.</span></ListItem>
+                    </List>
+                </span>
+            )
+        },
+        {
+
+            heading: 'How do I get Certified with A-CSM Certification?',
+            para: (
+                <span>
+
+
+                    Though passing the Advanced Scrum Master certification exam is a way to get certified as Advanced Scrum Master but it also involves few processes to have an evaluated and analysed approach.
+                    <List>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>Register at AgileWaters consulting for a 2 day A-CSM training class.</ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>Understand the fundamentals of Scrum Framework, roles and responsibility of advance scrum master and fundamentals of working by getting trained under a certified trainer in a two-day workshop class.</ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>After completion of the course, submit your A-CSM certificate license to the ScrumAlliance.</ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>Complete all the pre or post-work necessary things and get approved with a learning objective from the certified educator.</ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>Get your A-CSM license agreement and make sure to update and complete your Scrum Alliance membership profile.</ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>And once your license agreement is accepted, your Advanced Scrum Master membership profile is completed with 12 months of membership with Scrum Alliance and designation as Advanced Certified Scrum Master.</ListItem>
+                    </List>
+                </span>)
+        },
+        {
+            heading: 'What Skill will I acquire if I take an A- CSM Certification training course ? ',
+            para: (
+                <span>
+                    <List>
+
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>You will learn how to respond confidently to various problems in an organisation when encountering resistance to change, lack of engagement, low motivation, and unavailability of key people.</ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>You will learn the techniques to help your enterprise for how to adapt the Scrum framework.</ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>You will learn the methods with which you will be able to increase the engagement of the development team, scrum team and various sections for greater accountability, commitment, and buy -in.</ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>You will learn to scale the Scrum Framework and agile methods to various teams to make the cross - functional team dependencies much efficient and result oriented.</ListItem>
+                        <ListItem alignItems='flex-start'>
+                            <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon>You will learn the techniques to resolve the impediments quickly and will have the facilitated method for dialogue delivery with consumers, Scrum team, Product Owners, Stakeholders, Executives etc.</ListItem>
+                    </List>
+                </span>
+            )
+        },
+        {
+            heading: 'What are the prerequisites of Joining the A-CSM Certification training class?',
+            para: (
+                <span>
+
+                    There are no specific criteria to join the A-CSM Certification training class. The class is best in use for those who are Scrum Masters, Program Manager, Product Manager, Agile Coach, Delivery Manager and want to extend their skill sets and want to beyond the simple concepts of scrum thus intending to expand the facilitation, interaction, coaching and team dynamics.
+                </span>
+            )
+        },
+        {
+            heading: 'What is the Cost of A-CSM Certification Training Course?',
+            para: 'The cost of Agile Coaching Certification is Rs.32k in India and for across the globe, it is $500. training course and certification included in the same. It keeps changing with the market rate, however, Agilewaters provides the best market rate with underlying values. For more information Please whatsapp us on +44 79782 52723 or +91 94218 09846 or mail us at: Support@Agilewaters.com.'
+        },
+        {
+            heading: 'Is an Advanced Scrum Master much in need?',
+            para: <span>
+                The framework of Scrum has been adopted in more than 70% of Big MNCs. This has led to an increase in the demand of certified Scrum Master. The Advanced Scrum Master is an expert Scrum Master who provides the solutions beyond the basic line to the organisation and is a key force behind the implementation, facilitation, project management.
+                <br></br><br></br>
+                So most of the big MNCs like John Deere, Schlumberger, TCS, Siemens, Infosys, Deutsche Bank, Master Card, BNY Mellon, Netflix open various exquisite positions for the advanced Scrum Master within an organisation.
+            </span>
+        },
+        {
+            heading: 'What is the responsibility of an Advanced Scrum Master?',
+            para: (<span>
+
+                Advanced Scrum Masters are the experienced team leaders who empower the progress of their team with appropriate guidance in the following ways:
+                <List>
+                    <ListItem alignItems='flex-start'>
+                        <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon><span><b>Facilitating the Meetings:</b> An Advanced Scrum Master facilitates every type of meetings and governs team planning.</span></ListItem>
+                    <ListItem alignItems='flex-start'>
+                        <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon><span><b>Facilitate Sprint planning, sprint review:</b> An Advanced Scrum Master facilitates the daily sprint planning and checks the regular status of sprint development in the sprint review phase.</span></ListItem>
+                    <ListItem alignItems='flex-start'>
+                        <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon><span><b>Daily Scrum and backlog refinement:</b> An Advanced Scrum Master checks in the chart to manage the work backlog, to-do list, work in progress and tasks to be done through Daily Scrum and backlog refinement method.</span></ListItem>
+                    <ListItem alignItems='flex-start'>
+                        <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon><span><b>Planning and releasing the projects retrospective:</b> An Advanced Scrum Master is the key force behind planning of high scale projects who further release the retrospective to its Scrum development team for the same.</span></ListItem>
+                    <ListItem alignItems='flex-start'>
+                        <KeyboardDoubleArrowRightIcon></KeyboardDoubleArrowRightIcon><span><b>Managing team dynamics:</b> An Advanced Scrum Master manages the cross-functional working and dependency of the team with visual management and acts as a servant leader to the organisation.</span></ListItem>
+                </List>
+            </span>
+            )
+        },
+        {
+            heading: 'What Makes a great Advanced Scrum Master?',
+            para: (<span>
+
+
+                An Advanced Scrum Master is someone who is an expert to thrive with good ethics and evaluate the work process through effective leadership management skills. Through his work experience and good knowledge of Scrum Framework, they must expertise on how to deliver quality results and solutions to the organisation with consistency.
+                <br></br><br></br>
+                The coordination with the Scrum team and that too with other Scrum Masters and other members of the organisation need to be transparent, frequent. And this is where an advanced Scrum Master needs to be confident in response to encountering resistance to change, lack of engagement, low motivation, and unavailability of key people.
+                <br></br><br></br>
+                As a Scrum Master is mostly valued as how he leads his team, an Advanced Scrum Master needs to sound proficient with his leadership skills within the organisation. They must help their respective team to clear backlogs and bottlenecks very quickly along with guiding them on how to become agile with their work process. Their expertise level should come in great use when facilitating the dialogue between the Product Owner, team members, customers, stakeholders, and executives.
+                <br></br><br></br>
+                Check out our other <Link href=""> Online Scrum Master Certification</Link>, <Link href="">Licensed Scrum Master Certification</Link> & <Link href="">Scrum Quiz</Link>.
+            </span>
+            )
+        }
+
     ]
     return (
         <div>
--- src/modules/ScrumMaster/index.js
+++ src/modules/ScrumMaster/index.js
@@ -1,30 +1,29 @@
 import React from 'react'
-import HeroAdvScrum from './HeroAdvScrum'
-import { Container, Grid } from '@mui/material'
+import HeroSM from './HeroSM'
+import { Container, Grid, Typography } from '@mui/material'
 import { AppCard } from '@crema'
-import AdvScruminfo from './AdvScruminfo'
-import MainAdvSMaccord from './MainAdvSMaccord'
+import SMinfo from './SMinfo'
 import VideoMain from 'modules/commanmodules/VideoMain'
-import SideCard from 'modules/ScrumMasterCert/SideCard'
-import { sideList1 } from 'modules/Constant/OtherCardsConst'
 import Safetest from 'modules/SafeAgileCert/Safetest'
 import FreeCourseSlider from 'modules/commanmodules/FreeCourseSlider'
-import { freecourseList } from 'modules/Constant/FreeCourseConst'
-import Atscaledownload from 'modules/ScrumAtScale/Atscaledownload'
 import RelatedCourses from 'modules/commanmodules/RelatedCourses'
 import { safeadvList } from 'modules/Constant/Relatecoursedata'
+import { freecourseList } from 'modules/Constant/FreeCourseConst'
 
-export default function AdvScrumMaster() {
+export default function ScrumMaster() {
     return (
         <div>
-            <HeroAdvScrum />
+            <HeroSM />
             <Container sx={{ maxWidth: { xl: 1450 }, marginTop: 10 }}>
+                <h2>Scrum Master & Product Owner Certifications In Just 1 Online Training!</h2>
+                <Typography variant='h3' sx={{ color: "#2e6da4" }}> Get Certified as Scrum Master and Product Owner, endorsed by Scrum Inc. and Dr. Jeff Sutherland, the co-creator of Scrum!</Typography>
+                <br></br>
                 <Grid container spacing={{ xs: 4, md: 8 }}>
                     <Grid item xs={12} md={8}>
                         <AppCard sxStyle={{ p: 5 }}>
-                            <AdvScruminfo />
-                            <MainAdvSMaccord />
+                            <SMinfo />
                         </AppCard>
+
                     </Grid>
                     <Grid item xs={12} md={4}>
                         <VideoMain links="https://www.youtube.com/watch?v=rB9dlx8V480&t=2s" videoPromo={'sd'} />
