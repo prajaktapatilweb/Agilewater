b6eb686b30018216095b7b97a32e76aa5b0cb3b1
prajaktapatilweb <prajaktaweb@gmail.com>
2023-09-19 15:50:42 +0530

    page link updated

--- src/@crema/User/Userfooter.js
+++ src/@crema/User/Userfooter.js
@@ -1,78 +1,79 @@
-import {Container, Link} from '@mui/material';
+import { Container, Link } from '@mui/material';
 import React from 'react';
-import {Grid} from '@mui/material';
-import {Typography, Box} from '@mui/material';
+import { Grid } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import Image from 'next/image';
-import {Button} from '@mui/material';
+import { Button } from '@mui/material';
 import AppGridContainer from '@crema/core/AppGridContainer';
+import { contactlink, csmlink, devopsmasterlink, devopsproflink, pmiacplink, polink, safeagilelink, safesmlink, scrumatscalelink } from 'modules/commanmodules/Pagelinks';
 
 export default function Userfooter() {
   return (
     <div>
-      <Grid container className='section' style={{background: '#ffffff'}}>
+      <Grid container className='section' style={{ background: '#ffffff' }}>
         {/* backgroundImage: "linear-gradient(180deg,#9c27b0,#540162 )" */}
-        <Container sx={{maxWidth: {xl: 1450}}}>
+        <Container sx={{ maxWidth: { xl: 1450 } }}>
           <AppGridContainer>
             <Grid
               item
               xs={12}
               sm={6}
               md={3}
-              sx={{color: 'white'}}
-              ml={{xs: 10, sm: 0}}
+              sx={{ color: 'white' }}
+              ml={{ xs: 10, sm: 0 }}
             >
               <Link
-                href='/safepages/safe-agile-certification'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                href={scrumatscalelink}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 Scrum@Scale Practitioner
               </Link>
               <br></br>
               <Link
-                href='/safepages/safe-agile-certification'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                href={csmlink}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 CSM – Certified Scrum Master
               </Link>
               <br></br>
               <Link
-                href='/safepages/safe-agile-certification'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                href={safeagilelink}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 SAFe Agile Certification
               </Link>
               <br></br>
               <Link
-                href='/safepages/safe-agile-certification'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                href={safesmlink}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 SAFe Scrum Master
               </Link>
               <br></br>
               <Link
-                href='/safepages/safe-agile-certification'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                href={polink}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 Scrum Product Owner
               </Link>
               <br></br>
               <Link
-                href='/safepages/safe-agile-certification'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                href={devopsproflink}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 DevOps Professional
               </Link>
               <br></br>
               <Link
-                href='/safepages/safe-agile-certification'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                href={devopsmasterlink}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 DevOps Master
               </Link>
               <br></br>
               <Link
-                href='/safepages/safe-agile-certification'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                href={pmiacplink}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 PMI – ACP
               </Link>
@@ -82,13 +83,13 @@ export default function Userfooter() {
               xs={12}
               sm={6}
               md={3}
-              mb={{xs: 5, sm: 5, md: 0}}
-              ml={{xs: 10, sm: 0}}
+              mb={{ xs: 5, sm: 5, md: 0 }}
+              ml={{ xs: 10, sm: 0 }}
             >
               <Typography variant='h2' color='#20509e' gutterBottom>
                 Corporate Office
               </Typography>
-              <p style={{color: '#75849a'}}>
+              <p style={{ color: '#75849a' }}>
                 Office No.111, Vision 9 Mall,<br></br>
                 1st floor, Kunal Icon Rd,<br></br>
                 Pimple Saudagar, Pune, Maharashtra<br></br>
@@ -98,7 +99,7 @@ export default function Userfooter() {
               <Typography variant='h2' color='#20509e' gutterBottom>
                 Other Offices
               </Typography>
-              <p style={{color: '#75849a'}}>
+              <p style={{ color: '#75849a' }}>
                 Bengaluru, Mumbai, Noida,<br></br>
                 Hyderabad, Kolkata, Mexico, UK<br></br>
               </p>
@@ -109,19 +110,20 @@ export default function Userfooter() {
                   backgroundImage:
                     'linear-gradient(to right, #3e2bce 0%, #2dd3aa 100%, #2dd3aa 100%, #2dd3aa 100%)',
                 }}
+
               >
-                Contact Us
+                <Link href={contactlink} sx={{ color: 'white', textDecoration: 'none' }}>  Contact Us</Link>
               </Button>
             </Grid>
-            <Grid item xs={12} sm={6} md={3} ml={{xs: 10, sm: 0}}>
+            <Grid item xs={12} sm={6} md={3} ml={{ xs: 10, sm: 0 }}>
               <Typography variant='h2' color='#20509e' gutterBottom>
                 Reach Us
               </Typography>
-              <p style={{color: '#75849a'}} gutterBottom>
+              <p style={{ color: '#75849a' }} gutterBottom>
                 +91 9405045060<br></br>
                 +91 9421809846<br></br>
                 Support@AgileWaters.com<br></br>
-                AgileWaters-Linktree
+                <Link href='https://linktr.ee/Agilewaters'>AgileWaters-Linktree</Link>
               </p>
 
               <Typography variant='h2' color='#20509e' gutterBottom>
@@ -129,7 +131,7 @@ export default function Userfooter() {
               </Typography>
               <Link
                 href='https://www.apgionline.com/'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 {' '}
                 APGI
@@ -137,18 +139,18 @@ export default function Userfooter() {
               <br></br>
               <Link
                 href='https://vijaywade.com/'
-                sx={{color: '#00a1ff', textDecoration: 'none'}}
+                sx={{ color: '#00a1ff', textDecoration: 'none' }}
               >
                 {' '}
                 VijayWade
               </Link>
               <br></br>
             </Grid>
-            <Grid item xs={12} sm={6} md={3} ml={{xs: 10, sm: 0}}>
+            <Grid item xs={12} sm={6} md={3} ml={{ xs: 10, sm: 0 }}>
               <Typography variant='h2' color='#20509e' gutterBottom>
                 Legal
               </Typography>
-              <p style={{color: '#75849a'}} gutterBottom>
+              <p style={{ color: '#75849a' }} gutterBottom>
                 Privacy Policy<br></br>
                 Refund Policy<br></br>
               </p>
@@ -169,7 +171,7 @@ export default function Userfooter() {
                 width='100%'
               />
               <br></br>
-              <Box sx={{pl: 5, pt: 2}}>
+              <Box sx={{ pl: 5, pt: 2 }}>
                 <Image
                   alt='Safelogo'
                   src='/assets/images/safe/EXIN-Logo-e1625802522731.webp'
