70e38bdf6451bf40ed5a37d258772ed197d0da83
Prajakta_Laptop <prajaktaweb@gmail.com>
2023-11-26 16:37:53 +0530

    server copied to main folder

--- server/routes/api/coaches.js
+++ server/routes/api/coaches.js
@@ -6,134 +6,99 @@ const router = express.Router();
 const moment = require('moment');
 const CoachesList = require('../../models/CoachesList');
 const uploadController = require('../../middleware/uploadMultipleFiles');
-const Leads = require('../../models/Leads');
-const {client} = require('../../middleware/nodeemail');
-const Experts = require('../../models/Experts');
 
-// async function getCoachList(req, res) {
-//   console.log('In get Coach List');
-//   let NewList = await CoachesList.find({Status: 'Active'});
-//   console.log('first', NewList.length);
-//   return res.status(200).json({List: NewList});
-// }
-// router.get('/getcoachlist', auth, async (req, res) => {
-//   console.log('In request Get Coach List ');
+async function getCoachList(req, res) {
+  console.log('In get Coach List');
+  let NewList = await CoachesList.find({Status: 'Active'});
+  console.log('first', NewList.length);
+  return res.status(200).json({List: NewList});
+}
+router.get('/getcoachlist', auth, async (req, res) => {
+  console.log('In request Get Coach List ');
+  try {
+    // updateStatus();
+    getCoachList(req, res);
+    // let NewList = await CoachesList.find({ Status: "Active" });
+    // console.log("first", NewList.length);
+    // return res.status(200).json({ List: NewList });
+  } catch (err) {
+    // logger.error(`Catch Block - User List Request Block ${err}`, { by: req.user.gid, for: [0], info: {} })
+    console.log('Error ', err);
+    return res.status(500).json({error: `Server Error: ${err}`});
+  }
+});
+
+// router.get("/getindividualcourse", async (req, res) => {
+//   console.log("In request Get Indiv Course Data ", req.query);
+//   // res.setHeader("Access-Control-Allow-Origin", "*");
+
 //   try {
-//     // updateStatus();
-//     getCoachList(req, res);
-//     // let NewList = await CoachesList.find({ Status: "Active" });
-//     // console.log("first", NewList.length);
-//     // return res.status(200).json({ List: NewList });
+//     let CourseData = await CoursesList.findOne(
+//       { CourseID: req.query.CourseID },
+//       {
+//         CourseID: 1,
+//         CourseName: 1,
+//         EndDate: 1,
+//         EventDeleteDate: 1,
+//         Place: 1,
+//         ActualCost: 1,
+//         DiscountedCost: 1,
+//         StartDate: 1,
+//         Time: 1,
+//         Trainer: 1,
+//         _id: 0,
+//       }
+//     );
+
+//     console.log("first", CourseData);
+
+//     return res.status(200).json({ CourseData });
 //   } catch (err) {
 //     // logger.error(`Catch Block - User List Request Block ${err}`, { by: req.user.gid, for: [0], info: {} })
-//     console.log('Error ', err);
-//     return res.status(500).json({error: `Server Error: ${err}`});
+//     return res.status(500).json({ error: `Server Error: ${err}` });
 //   }
 // });
 
-router.post('/addnewlead', async (req, res) => {
-  console.log('In add new Course router post request', req.body);
+router.post('/addnewcoach', auth, async (req, res) => {
+  console.log('In add new Course router post request');
   try {
-    // const CoachID = req.params.ID;
-    const data = req.body;
-    async function asyncCall() {
-      let totalNumber = await Leads.countDocuments();
-      totalNumber = totalNumber >= 1 ? totalNumber + 1 : 1;
-      data.LeadID = `LeadID-${totalNumber}`;
-      // data.Avatar = req.Avatar;
-      const clientIp =
-        (req.headers['x-forwarded-for'] || '').split(',').pop().trim() ||
-        req.socket.remoteAddress;
-      console.log('IP', clientIp);
-      FinalData = new Leads(data);
+    uploadController.multipleUpload(req, res, function (err) {
+      if (err) {
+        console.log('Error Photo Submission', err);
+        return res.end('Error uploading file.');
+      }
+      // const CoachID = req.params.ID;
+      const data = req.body;
+      async function asyncCall() {
+        let totalNumber = await CoachesList.countDocuments();
+        totalNumber = totalNumber >= 1 ? totalNumber + 1 : 1;
+        data.CoachID = `ID-${totalNumber}`;
+        // data.Avatar = req.Avatar;
 
-      const emailContent = `Dear Admin, \n \n Details of the person contacted you are on the link ${data.EnquiryFromLink} as follows \n
-    Name: ${req.body.FirstName} ${data.LastName} \n
-    Email: ${req.body.Email} \n
-    Mobile Number: ${req.body.Phone} \n
-    City & Country : ${req.body.City}, ${data.Country} \n
-    Message : ${req.body.Message} \n
-    Date & Time :${FinalData.Created.OnDate} \n
-    IP Address: ${clientIp} \n`;
+        data.Created = {};
+        data.Created.ByID = req.user.gid;
+        data.Created.ByName = req.user.name;
 
-      await client.sendMail({
-        from: `Web Developer <${process.env.EMAILID}>`,
-        to: 'prajubpatil@gmail.com',
-        subject: 'emailSubject',
-        text: emailContent,
-      });
-
-      console.log('Final Data', FinalData);
-      await FinalData.save()
-        .then(() => {
-          return res.status(200).json({data: 'Success'});
-        })
-        .catch((err) => {
-          console.log('Errot', err);
-          return res
-            .status(500)
-            .json({error: `Problem in Storing to MongoDB: ${err}`});
-        });
-    }
-    asyncCall();
+        FinalData = new CoachesList(data);
+        console.log('Final Data', FinalData);
+        await FinalData.save()
+          .then(() => {
+            return res.status(200).json({data: 'Success'});
+          })
+          .catch((err) => {
+            console.log('Errot', err);
+            return res
+              .status(500)
+              .json({error: `Problem in Storing to MongoDB: ${err}`});
+          });
+      }
+      asyncCall();
+    });
   } catch (err) {
     console.log('Error', err);
     return res.status(500).json({error: `Server Error: ${err}`});
   }
 });
-
-/*add by praju */
-router.post('/addnewexpert', async (req, res) => {
-  console.log('In add new expert data request', req.body);
-  try {
-    // const CoachID = req.params.ID;
-    const data = req.body;
-    async function asyncCall() {
-      let totalNumber = await Experts.countDocuments();
-      totalNumber = totalNumber >= 1 ? totalNumber + 1 : 1;
-      data.ExpertID = `ExpertID-${totalNumber}`;
-      // data.Avatar = req.Avatar;
-      const clientIp =
-        (req.headers['x-forwarded-for'] || '').split(',').pop().trim() ||
-        req.socket.remoteAddress;
-      console.log('IP', clientIp);
-      FinalData = new Experts(data);
-
-      const emailContent = `Dear Admin, \n \n Details of the person contacted you are on the link ${data.EnquiryFromLink} as follows \n
-    Name: ${req.body.FirstName} \n
-    Email: ${req.body.Email} \n
-    Mobile Number: ${req.body.Phone} \n
-    Certification : ${req.body.Certification}, \n
-    Message : ${req.body.Message} \n
-    Date & Time :${FinalData.Created.OnDate} \n
-    IP Address: ${clientIp} \n`;
-
-      await client.sendMail({
-        from: `Web Developer <${process.env.EMAILID}>`,
-        to: 'prajubpatil@gmail.com',
-        subject: 'emailSubject',
-        text: emailContent,
-      });
-
-      console.log('Final Data', FinalData);
-      await FinalData.save()
-        .then(() => {
-          return res.status(200).json({data: 'Success'});
-        })
-        .catch((err) => {
-          console.log('Errot', err);
-          return res
-            .status(500)
-            .json({error: `Problem in Storing to MongoDB: ${err}`});
-        });
-    }
-    asyncCall();
-  } catch (err) {
-    console.log('Error', err);
-    return res.status(500).json({error: `Server Error: ${err}`});
-  }
-});
-/*end  */
 
 router.put('/updatecoach/:CoachID', auth, async (req, res) => {
   try {
