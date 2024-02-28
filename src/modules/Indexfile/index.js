449efcd74104c72417c11f6efad88b7d91233152
prajaktapatilweb <prajaktaweb@gmail.com>
2024-02-28 15:10:14 +0530

    indexdemo2 map section using flexbox

--- src/modules/Indexfile/index.js
+++ src/modules/Indexfile/index.js
@@ -131,6 +131,114 @@ export default function indexfile() {
 
       {/* <Hero /> */}
 
+      {/* <Grid
+                        container
+                        spacing={0}
+                        alignItems='center'
+                        justifyContent='center'
+                    >
+                        <Grid
+                            item
+                            xs={12}
+                            sm={12}
+                            md={5}
+                            lg={5}
+                            sx={{ mt: { xs: 20, sm: 20, md: 20, lg: 0 } }}
+                        >
+                            <Box>
+                                <Typography
+                                    sx={{
+                                        // width: { md: 850 },
+                                        position: 'relative',
+                                        // fontSize: { xs: 30, md: 40, lg: 40 },
+                                        mb: { xs: 2, sm: 0 },
+                                        letterSpacing: 1.5,
+
+                                        color: 'white',
+                                        lineHeight: 1.5,
+                                    }}
+                                >
+                                    <span className='h8'>
+                                        Global Training, Certification & Recruitment Expert
+                                    </span>
+                                </Typography>
+                            </Box>
+
+                            <Box sx={{ mb: 5, width: { xs: '100%', md: '80%' } }}>
+                                <Typography
+                                    variant='p'
+                                    sx={{ color: 'white', lineHeight: 1.6, fontSize: 18 }}
+                                >
+                                    {'Helps you from Learning to earning (Value generation)'}
+                                </Typography>
+                            </Box>
+
+                            <Button
+                                color='primary'
+                                size='large'
+                                variant='contained'
+                                sx={{
+                                    // my: { xs: 1, sm: 1, md: 0 },
+
+                                    // fontSize: 17,
+                                    backgroundColor: '#fff',
+                                    // p: 4,
+                                    fontWeight: 'bold',
+                                    boxShadow: 'rgba(0, 0, 0, 0.17) 0px -13px 15px 0px inset, rgba(0, 0, 0, 0.5) 0px -6px 3px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px',
+                                    color: '#3e2ecd',
+                                }}
+                            >
+                                In Demand Course
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={7} lg={7}>
+                            <Box className='world-map'>
+                                <Image
+                                    src='/assets/images/test6-min.webp'
+                                    width={100}
+                                    height={100}
+                                    objectFit='contain'
+                                    sizes="100vw"
+                                    loading='eager'
+                                    priority
+                                    alt='worldmap'
+                                ></Image>
+                                {/* <img src="assets/images/test8.png" alt=''></img> *
+                                <Box className='pin usa'>
+                                    <span>
+                                        San Francisco<br></br>
+                                        California -USA
+                                    </span>
+                                </Box>
+                                <Box className='pin abc'>
+                                    <span></span>
+                                </Box>
+                                <Box className='pin france'>
+                                    <span></span>
+                                </Box>
+                                <Box className='pin sweden'>
+                                    <span></span>
+                                </Box>
+                                <Box className='pin pqr'>
+                                    <span></span>
+                                </Box>
+                                <Box className='pin india'>
+                                    <span>
+                                        Pune <br></br> India
+                                    </span>
+                                </Box>
+                                <Box className='pin south-africa'>
+                                    <span></span>
+                                </Box>
+                                <Box className='pin australia'>
+                                    <span></span>
+                                </Box>
+                            </Box>
+
+                            {/* <Image src="/assets/images/test6.png" width={100} height={100} quality={97} layout='responsive' objectFit='contain' alt="Feature img" /> *
+                        </Grid>
+                    </Grid> */}
+
 
     </div >
   );
